4a4cdf17c5f1a7c072b319d48a38e38ff29b98fd
ivan555-lab <vnchapay@gmail.com>
2022-07-07 08:52:26 +0300

    ch2

--- shop/models.py
+++ shop/models.py
@@ -3,3 +3,6 @@ from django.db import models
 #dev1 form w_st1 form a1 will develop here
 
 class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    

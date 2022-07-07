f913f9e201ffb3c89d1f2f006a023f6f7520058d
ivan555-lab <vnchapay@gmail.com>
2022-07-07 09:05:28 +0300

    ch3

--- shop/models.py
+++ shop/models.py
@@ -5,4 +5,13 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categores'
+
+    def __str__(self):
+        return self.name
+
     

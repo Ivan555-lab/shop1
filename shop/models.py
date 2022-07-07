7e779be881452cf63fa0645228314040b4e611aa
ivan555-lab <vnchapay@gmail.com>
2022-07-07 09:12:00 +0300

    ch4

--- shop/models.py
+++ shop/models.py
@@ -14,4 +14,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)            )
